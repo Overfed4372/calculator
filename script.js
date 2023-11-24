ec00c4336975e4b974add15c214a85566bc254d4
Ahmad <ahmad@test.example>
2023-11-24 11:21:21 -0330

    Add display altering to buttons function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//Selecting numbers, digits and display buttons
 const numbersButton = [...document.querySelectorAll(".number")];
+const digitsButton = [...document.querySelectorAll(".digit")];
+const displayContent = document.querySelector(".display");
+const clearButton = document.querySelector(".clear");
+
+//Adding the same event listener to both numbers and digits button for altering display 
+numbersButton.concat(digitsButton).forEach ( (element) => {
+    element.addEventListener ('click' , (pressed) => {
+        displayContent.textContent += pressed.target.textContent;
+    })
+});
+clearButton.addEventListener ('click' , () => {
+    displayContent.textContent = "";
+});
